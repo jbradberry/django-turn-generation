de6f5b797822dde4f9cfae7be6ed9b6e458873ce
Jeff Bradberry <jeff.bradberry@gmail.com>
2015-02-15 15:39:25 -0500

    Split out a RealmMixin class.

--- turngeneration/views.py
+++ turngeneration/views.py
@@ -33,24 +33,27 @@ logger = logging.getLogger(__name__)
 # /api/starsgame/3/starsrace/5/ready/
 
 
-class RealmListView(generics.ListAPIView):
-    # /api/starsgame/
-    serializer_class = serializers.RealmSerializer
-
-    def get_queryset(self):
-        content_type = plugins.realm_type(self.kwargs.get('realm_alias'))
-        if content_type is None:
-            raise Http404
-        return content_type.model_class().objects.all()
-
-
-class RealmRetrieveView(generics.RetrieveAPIView):
-    # /api/starsgame/3/
+class RealmMixin(object):
     serializer_class = serializers.RealmSerializer
     lookup_url_kwarg = 'realm_pk'
+    type_url_kwarg = 'realm_alias'
+
+    def get_type_kwarg(self):
+        return self.type_url_kwarg
 
     def get_queryset(self):
-        content_type = plugins.realm_type(self.kwargs.get('realm_alias'))
+        alias = self.kwargs.get(self.get_type_kwarg())
+        content_type = plugins.realm_type(alias)
         if content_type is None:
             raise Http404
         return content_type.model_class().objects.all()
+
+
+class RealmListView(RealmMixin, generics.ListAPIView):
+    # /api/starsgame/
+    pass
+
+
+class RealmRetrieveView(RealmMixin, generics.RetrieveAPIView):
+    # /api/starsgame/3/
+    pass
