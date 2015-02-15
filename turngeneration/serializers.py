a565235303e1f2572ed34490e25c7e0f31aba74c
Jeff Bradberry <jeff.bradberry@gmail.com>
2015-02-15 16:26:43 -0500

    Support nested generator inside the realm.

--- turngeneration/serializers.py
+++ turngeneration/serializers.py
@@ -5,15 +5,55 @@ from . import models
 
 
 class ContentTypeField(serializers.Field):
-    def to_representation(self, obj):
-        ct = ContentType.objects.get_for_model(obj)
-        return u'{ct.app_label}.{ct.model}'.format(ct=ct)
+    def to_representation(self, value):
+        return u'{value.app_label}.{value.model}'.format(value=value)
 
-    def get_attribute(self, obj):
-        return obj
+    def to_internal_value(self, data):
+        app_label, model = data.split('.')
+        return ContentType.objects.get_by_natural_key(app_label, model)
+
+
+class ReadOnlyDefault(object):
+    def set_context(self, serializer_field):
+        self.current_value = getattr(serializer_field.parent.instance,
+                                     serializer_field.name, None)
+
+    def __call__(self):
+        return self.current_value
+
+    def __repr__(self):
+        return '%s()' % (self.__class__.__name__,)
+
+
+class GeneratorSerializer(serializers.ModelSerializer):
+    content_type = ContentTypeField(read_only=True, default=ReadOnlyDefault())
+    object_id = serializers.IntegerField(read_only=True,
+                                         default=ReadOnlyDefault())
+
+    class Meta(object):
+        model = models.Generator
+        fields = ('content_type', 'object_id',
+                  'generating', 'generation_time', 'autogenerate',
+                  'allow_pauses', 'minimum_between_generations')
+        read_only_fields = ('generating', 'generation_time')
 
 
 class RealmSerializer(serializers.Serializer):
-    content_type = ContentTypeField()
+    content_type = serializers.SerializerMethodField()
     object_id = serializers.IntegerField(source='pk')
     repr = serializers.CharField(source='__repr__')
+    generator = serializers.SerializerMethodField(required=False)
+
+    def get_content_type(self, obj):
+        ct = ContentType.objects.get_for_model(obj)
+        return u'{ct.app_label}.{ct.model}'.format(ct=ct)
+
+    def get_generator(self, obj):
+        ct = ContentType.objects.get_for_model(obj)
+        try:
+            generator = models.Generator.objects.get(
+                content_type=ct, object_id=obj.pk)
+        except models.Generator.DoesNotExist:
+            return None
+
+        return GeneratorSerializer(generator).data
