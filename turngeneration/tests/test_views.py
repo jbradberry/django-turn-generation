9642e1ccc01cf87f47d3201e9747fd1c6c7ec7c4
Jeff Bradberry <jeff.bradberry@gmail.com>
2015-02-17 21:29:53 -0500

    Added more Ready tests.

--- turngeneration/tests/test_views.py
+++ turngeneration/tests/test_views.py
@@ -208,8 +208,141 @@ class ReadyViewTestCase(TestCase):
         self.agent.save()
         self.client.login(username='test', password='password')
 
+    def test_realm_does_not_exist(self):
+        self.assertEqual(models.Ready.objects.count(), 0)
+
+        realm_url = reverse('ready',
+                            kwargs={'realm_alias': 'testrealm',
+                                    'realm_pk': self.realm.pk + 1,
+                                    'agent_alias': 'testagent',
+                                    'agent_pk': self.agent.pk})
+
+        response = self.client.get(realm_url)
+        self.assertEqual(response.status_code, 404)
+
+        response = self.client.post(realm_url, follow=True)
+        self.assertEqual(response.status_code, 404)
+
+        response = self.client.delete(realm_url, follow=True)
+        self.assertEqual(response.status_code, 404)
+
+        self.assertEqual(models.Ready.objects.count(), 0)
+
+    def test_generator_does_not_exist(self):
+        self.assertEqual(models.Ready.objects.count(), 0)
+
+        realm_url = reverse('ready',
+                            kwargs={'realm_alias': 'testrealm',
+                                    'realm_pk': self.realm.pk,
+                                    'agent_alias': 'testagent',
+                                    'agent_pk': self.agent.pk})
+
+        response = self.client.get(realm_url)
+        self.assertEqual(response.status_code, 404)
+
+        response = self.client.post(realm_url, follow=True)
+        self.assertEqual(response.status_code, 404)
+
+        response = self.client.delete(realm_url, follow=True)
+        self.assertEqual(response.status_code, 404)
+
+        self.assertEqual(models.Ready.objects.count(), 0)
+
+    def test_agent_does_not_exist(self):
+        self.assertEqual(models.Ready.objects.count(), 0)
+
+        generator = models.Generator(content_object=self.realm)
+        generator.save()
+
+        realm_url = reverse('ready',
+                            kwargs={'realm_alias': 'testrealm',
+                                    'realm_pk': self.realm.pk,
+                                    'agent_alias': 'testagent',
+                                    'agent_pk': self.agent.pk + 1})
+
+        response = self.client.get(realm_url)
+        self.assertEqual(response.status_code, 404)
+
+        response = self.client.post(realm_url, follow=True)
+        self.assertEqual(response.status_code, 404)
+
+        response = self.client.delete(realm_url, follow=True)
+        self.assertEqual(response.status_code, 404)
+
+        self.assertEqual(models.Ready.objects.count(), 0)
+
+    def test_user_does_not_have_permission(self):
+        self.assertEqual(models.Ready.objects.count(), 0)
+
+        generator = models.Generator(content_object=self.realm)
+        generator.save()
+
+        realm_url = reverse('ready',
+                            kwargs={'realm_alias': 'testrealm',
+                                    'realm_pk': self.realm.pk,
+                                    'agent_alias': 'testagent',
+                                    'agent_pk': self.agent.pk})
+
+        response = self.client.get(realm_url)
+        self.assertEqual(response.status_code, 404)
+
+        response = self.client.post(realm_url, follow=True)
+        self.assertEqual(response.status_code, 403)
+
+        response = self.client.delete(realm_url, follow=True)
+        self.assertEqual(response.status_code, 403)
+
+        self.assertEqual(models.Ready.objects.count(), 0)
+
+    def test_success(self):
+        self.assertEqual(models.Ready.objects.count(), 0)
+
+        generator = models.Generator(content_object=self.realm)
+        generator.save()
+        self.agent.user = self.user
+        self.agent.save()
+
+        realm_url = reverse('ready',
+                            kwargs={'realm_alias': 'testrealm',
+                                    'realm_pk': self.realm.pk,
+                                    'agent_alias': 'testagent',
+                                    'agent_pk': self.agent.pk})
+
+        response = self.client.get(realm_url)
+        self.assertEqual(response.status_code, 404)
+
+        response = self.client.post(realm_url, follow=True)
+        self.assertEqual(response.status_code, 201)
+
+        self.assertEqual(models.Ready.objects.count(), 1)
+
     def test_already_ready(self):
-        pass
+        generator = models.Generator(content_object=self.realm)
+        generator.save()
+        self.agent.user = self.user
+        self.agent.save()
+
+        ready = models.Ready(agent=self.agent, generator=generator)
+        ready.save()
+        self.assertEqual(models.Ready.objects.count(), 1)
+
+        realm_url = reverse('ready',
+                            kwargs={'realm_alias': 'testrealm',
+                                    'realm_pk': self.realm.pk,
+                                    'agent_alias': 'testagent',
+                                    'agent_pk': self.agent.pk})
+
+        response = self.client.get(realm_url)
+        self.assertEqual(response.status_code, 200)
+
+        response = self.client.post(realm_url, follow=True)
+        self.assertContains(response,
+            "The fields content_type, object_id, generator must make a"
+            " unique set.",
+            status_code=400
+        )
+
+        self.assertEqual(models.Ready.objects.count(), 1)
 
     def test_can_mark_ready_while_paused(self):
         pass
