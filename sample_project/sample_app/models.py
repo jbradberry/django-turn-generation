c3e157cfefe9db3ca99601f5968ac7659294ba8f
Jeff Bradberry <jeff.bradberry@gmail.com>
2019-12-11 20:39:00 -0500

    Fix problems with the models
    - on_delete is now mandatory for relational fields
    - CommaSeparatedIntegerField has been deprecated and removed, in favor
      of CharField plus a validator

--- sample_project/sample_app/models.py
+++ sample_project/sample_app/models.py
@@ -12,6 +12,6 @@ class TestRealm(models.Model):
 
 
 class TestAgent(models.Model):
-    realm = models.ForeignKey(TestRealm, related_name='agents', null=True)
+    realm = models.ForeignKey(TestRealm, on_delete=models.SET_NULL, related_name='agents', null=True)
     slug = models.CharField(max_length=16)
-    user = models.ForeignKey('auth.User', null=True)
+    user = models.ForeignKey('auth.User', on_delete=models.SET_NULL, null=True)
